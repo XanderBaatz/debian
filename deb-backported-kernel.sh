f9bb8f38d5a7eb3979ecb2f5e7a4e7c10ccd8499
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-11-02 15:24:41 +0100

    Update deb-backported-kernel.sh

--- deb-backported-kernel.sh
+++ deb-backported-kernel.sh
@@ -5,16 +5,20 @@ set -e
 
 #variables
 _arch=$(dpkg --print-architecture)
+ver_name=$(cat /etc/*-release | grep VERSION_CODENAME | cut -f2 -d'=')
 i_pkg="linux-image-${_arch} firmware-linux firmware-linux-nonfree"
 nm_conf="/etc/NetworkManager/NetworkManager.conf"
+
+#update sources and check if backports are enabled
+if [ $(sudo apt update | grep "${ver_name}-backports"; echo $?) != "0" ]; then
+  echo "Unable to install ${i_pkg} , aborting."
+  exit $1
+fi
 
 #install network-manager
 sudo apt install --no-install-recommends --no-install-suggests -y ${i_pkg}
 
 #exit if network-manager isn't installed
-if [ $(dpkg-query -W -f='${Status}' ${i_pkg} | grep -q -P '^install ok installed$'; echo $?) != "0" ]; then
-  echo "Unable to install ${i_pkg} , aborting."
-  exit $1
-fi
+
 
 
