ef5cbeb20a3b77e614e332f8fb430e88c18c6417
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-11-02 17:41:31 +0100

    Update deb-backported-kernel.sh

--- deb-backported-kernel.sh
+++ deb-backported-kernel.sh
@@ -9,6 +9,16 @@ ver_name=$(cat /etc/*-release | grep VERSION_CODENAME | cut -f2 -d'=')
 i_pkg="linux-image-${_arch} firmware-linux firmware-linux-nonfree"
 
 #update sources and check if backports are enabled
+read -p "Do you want to continue? [Y/n] " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+		echo "You said no, aborting..."
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
+echo "You said yes, continuing..."
+
 if [ $(sudo apt update | grep "${ver_name}-backports"; echo $?) != "0" ]; then
   echo "Unable to install ${i_pkg} , aborting."
 fi
