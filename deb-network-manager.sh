30bfc3311a95ffbe8eb19a831f0d5533659f3c96
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-11-02 14:39:25 +0100

    Update deb-network-manager.sh

--- deb-network-manager.sh
+++ deb-network-manager.sh
@@ -27,5 +27,6 @@ sudo systemctl restart NetworkManager.service
 #enable networkmanager devices to make use of ifupdown, and fix "connected (externally)"
 for d in $(nmcli -t dev | awk '/unmanaged/ && !/loopback/' | cut -f1 -d':'); do
   sudo nmcli dev set $d managed no;
-  sudo nmcli dev set $d managed yes
+  sudo nmcli dev set $d managed yes;
+  sudo nmcli device reapply $d
 done
