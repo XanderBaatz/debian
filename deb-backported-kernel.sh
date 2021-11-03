e60c8e684b810b3a0f43d247512b670c48388361
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-11-03 15:48:11 +0100

    Update deb-backported-kernel.sh

--- deb-backported-kernel.sh
+++ deb-backported-kernel.sh
@@ -19,17 +19,27 @@ bp_check=$(egrep -v '^#|^ *$' /etc/apt/sources.list | grep -q ${ver_name}-backpo
 #$(sudo apt update | grep "${ver_name}-backports"; echo $?)
 if [ ${bp_check} != "0" ]; then
     echo "Debian ${ver_name}-backports not enabled."
-    read -p "Do you want to enable and continue? [Y/n] " -n 1 -r
-    echo
-    if [[ ! $REPLY =~ ^[Yy]$ ]]
-    then
-        echo "Abort."
-        [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
-    else
-        sudo echo "deb ${repo_url} ${ver_name}-backports ${component}" >> /etc/apt/sources.list 2>&1 && \
-        sudo apt update -y
-    fi
+    echo -n "Do you want to enable and continue? [Y/n] "
+    read answer
+        if [ "$answer" != "${answer#[Yy]}" ] ;then 
+            echo "Abort."
+            exit 1
+        else
+            sudo echo "deb ${repo_url} ${ver_name}-backports ${component}" >> /etc/apt/sources.list 2>&1 && \
+            sudo apt update -y
+        fi
 fi
+
+#    read -p "Do you want to enable and continue? [Y/n] " -n 1 -r
+#    echo
+#    if [[ ! $REPLY =~ ^[Yy]$ ]]
+#    then
+#        echo "Abort."
+#        [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+#    else
+#        sudo echo "deb ${repo_url} ${ver_name}-backports ${component}" >> /etc/apt/sources.list 2>&1 && \
+#        sudo apt update -y
+#    fi
 
 #install backported kernel and firmware
 sudo apt install -t ${ver_name}-backports -y ${i_pkg}
