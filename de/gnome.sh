b7100ffff25a381a73e2269aeb9a04eb68b0e5f8
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-11-06 09:00:44 +0100

    Update gnome.sh

--- de/gnome.sh
+++ de/gnome.sh
@@ -1,1 +1,10 @@
-#!/usr
+#!/usr/bin/env bash
+# wget -qO- https://git.io/????? | sudo sh
+
+set -e
+
+#variables
+_arch=$(dpkg --print-architecture)
+ver_name=$(cat /etc/*-release | grep VERSION_CODENAME | cut -f2 -d'=')
+repo_url=$(cat /etc/apt/sources.list | grep -o "deb http[^']\+${dist_name}" | head -1 | cut -f2 -d' ')
+component="main contrib non-free"
