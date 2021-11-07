31c2ca422ba89f0367598ecc696ad6bccb3d0579
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-11-07 22:14:50 +0100

    Update gnome-extra.sh

--- de/gnome-extra.sh
+++ de/gnome-extra.sh
@@ -6,6 +6,11 @@ sudo apt install -y alien
 
 #extensions in rpm
 popshell="gnome-shell-extension-pop-shell"
+rpm_get="$(wget -qO- "https://rpmfind.net/linux/rpm2html/search.php?query=${popshell}&system=opensuse&arch=noarch" | grep -Po "(?<=href=')[^']*.rpm" | head -1)"
+wget -N https://rpmfind.net${rpm_get}
+
+#extensions in rpm
+popshell="gnome-shell-extension-pop-shell"
 rpm_get="$(wget -qO- https://rpmfind.net/linux/rpm2html/search.php?query=${popshell}&system=opensuse&arch=noarch | grep -Po "(?<=href=')[^']*.rpm" | head -1)"
 wget -N https://rpmfind.net${rpm_get}
 
