3b7deb13407d160de81d9d4e5a3c9b2780b7c842
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-11-07 11:09:20 +0100

    Update deb-stable-rolling.sh

--- deb-stable-rolling.sh
+++ deb-stable-rolling.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# wget -qO- https://git.io/JPjey | sudo sh
+# wget -qO- https://git.io/JXGQl | sudo sh
 # https://www.linuxcapable.com/how-to-install-or-upgrade-to-linux-kernel-5-14-on-debian-11-bullseye/
 
 set -e
