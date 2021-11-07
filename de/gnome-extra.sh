99bc0f2875bf43ba16907aaad1cb255a0c368d9a
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-11-07 21:59:06 +0100

    Create gnome-extra.sh

---
+++ de/gnome-extra.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# wget -qO-  | sudo sh
+
+#rpm to deb conversion tools (temporary)
+sudo apt install -y alien
+
+#extensions in rpm
+popshell="gnome-shell-extension-pop-shell"
+wget -qO- https://rpmfind.net/linux/rpm2html/search.php?query=${popshell}&system=opensuse&arch=noarch \
+| grep -Po "(?<=href=')[^']*.rpm" | head -1
+
+#convert rpm package to deb
+sudo alien -d ${popshell}*.rpm && sudo alien -i ${popshell}*.deb
+sudo apt autoremove --purge alien -y
+
+#enable installed gnome extensions
+for e in $(gnome-extensions list); do
+  gnome-extensions enable $e
+done
